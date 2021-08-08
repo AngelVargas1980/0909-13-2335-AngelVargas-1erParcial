e3ed1e6fc8b0277309afc41a8280c1b98cc895e3
AngelVargas1980 <49385201+AngelVargas1980@users.noreply.github.com>
2021-08-07 20:38:46 -0600

    Cree el metodo rCuadrada en la clase CalculadoraAvanzada y agregue una validacion que el numero ingresado sea >=0, de lo contrario sera indefinido.

--- src/Clases/CalculadoraAvanzada.java
+++ src/Clases/CalculadoraAvanzada.java
@@ -4,6 +4,30 @@ public class CalculadoraAvanzada {
 
 
     public void rCuadrada() {
+        //Variables
+        float num = 0;
+        int seguir = 0;
+        do {
+            System.out.println("Calcular la raíz cuadrada de un número.");
+
+            System.out.println("\nIngrese el numero para calcular su Raiz Cuadrada.");
+            num = ScannerText.datosFloat();
+
+            if (num < 0){
+                System.out.println("INDEFINIDO");
+            }else {
+                //usamos las librerias MATH con el metodo sqrt para sacar la Raiz.
+                double resultado = Math.sqrt(num);
+                System.out.println("La raíz cuadrada de " + num + " es " + resultado);
+            }
+
+            System.out.println("\n¿Desea volver a calcular otra Raiz?");
+            System.out.println("1) Sí.");
+            System.out.println("2) No, salir.");
+            seguir = ScannerText.datosInt();
+            //Si elije si, incializamos en 0 las variables.
+            num = 0;
+        } while (seguir != 2);
     }
 
     public void potencias() {
